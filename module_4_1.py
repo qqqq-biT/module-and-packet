6220d60fa840f0fffc5da59431327a93f0019a7c
Absolute <187764974+qqqq-biT@users.noreply.github.com>
2024-12-15 19:00:42 +0300

    Update module_4_1.py

--- module_4_1.py
+++ module_4_1.py
@@ -1,1 +1,11 @@
+from fake_math import divide as fake_dv
+from true_math import divide as true_dv
 
+
+error = fake_dv(59, 3)
+error_1 = fake_dv(9, 0)
+infinity = true_dv(69, 3)
+infinity_1 = true_dv(9, 0)
+
+print(f"Ответ №1: {error}\nОтвет №1.2: {error_1}")
+print(f"Ответ №2: {infinity}\nОтвет №2.2: {infinity_1}")
